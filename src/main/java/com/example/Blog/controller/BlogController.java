9c4f2cef95fc5f51b192639c4abde1777afa29c8
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-27 14:49:04 +0100

    returning a list of posts and mapping to the path /posts

--- src/main/java/com/example/Blog/controller/BlogController.java
+++ src/main/java/com/example/Blog/controller/BlogController.java
@@ -1,15 +1,28 @@
 package com.example.Blog.controller;
 
 
+import com.example.Blog.entities.Post;
+import com.example.Blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
 
+@RestController
 public class BlogController {
+
+    @Autowired
+    private PostService postService;
 
     @GetMapping(value="/")
     public String index() {
         return "index";
+    }
+
+//    return a list of posts and it will be mapped to the path /posts.
+    @GetMapping(value="/posts")
+    public List<Post> posts() {
+        return postService.getAllPosts();
     }
 }
--- src/main/java/com/example/Blog/service/PostService.java
+++ src/main/java/com/example/Blog/service/PostService.java
@@ -1,8 +1,25 @@
 package com.example.Blog.service;
 
+import com.example.Blog.entities.Post;
+import com.example.Blog.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+//this will interact with our repo
+
 
 @Service
 public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+
+    }
 
 }
