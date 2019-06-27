20c836efe7e0ed4ded2a3dfed1803a8a55067268
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-27 14:31:52 +0100

    extending the JPA repo interface in order for it to work with the Post class entity

--- src/main/java/com/example/Blog/repositories/PostRepository.java
+++ src/main/java/com/example/Blog/repositories/PostRepository.java
@@ -1,4 +1,12 @@
 package com.example.Blog.repositories;
 
-public interface PostRepository {
+import com.example.Blog.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+//it is extending the JPA repo interface to make it work with the post class of entity  which we created before.
+//its going to use a Long as its identifier which is what we created below.
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+    
 }
