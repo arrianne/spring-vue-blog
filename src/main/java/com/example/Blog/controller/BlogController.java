5a286cf2c7191088fe63638632135fcc1d5a5931
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-27 13:55:21 +0100

    initial commit

---
+++ src/main/java/com/example/Blog/controller/BlogController.java
@@ -0,0 +1,15 @@
+package com.example.Blog.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class BlogController {
+
+    @GetMapping(value="/")
+    public String index() {
+        return "index";
+    }
+}
