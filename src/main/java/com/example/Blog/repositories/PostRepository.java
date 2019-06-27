5fb3a5dda627bd586e2bd28a90ae014929eb0049
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-27 14:36:18 +0100

    added new service package which will interact with our repo

--- src/main/java/com/example/Blog/repositories/PostRepository.java
+++ src/main/java/com/example/Blog/repositories/PostRepository.java
@@ -2,11 +2,13 @@ package com.example.Blog.repositories;
 
 import com.example.Blog.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 //it is extending the JPA repo interface to make it work with the post class of entity  which we created before.
 //its going to use a Long as its identifier which is what we created below.
 
+@Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
-    
+
 }
---
+++ src/main/java/com/example/Blog/service/PostService.java
@@ -0,0 +1,8 @@
+package com.example.Blog.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+}
